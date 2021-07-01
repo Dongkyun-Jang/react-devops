98d9e762ecbcb1a834a9b4afd56ee18e69aa07fb
Dongkyun_Jang <thd6576@naver.com>
2021-07-02 01:31:27 +0900

    build: first deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./redux/counter";
 
-function App() {
+export default function App() {
+  const { count } = useSelector((state) => state.counter); // 여기서 state는 store.js의 reducer 객체를 의미한다.
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Count is : {count}</h1>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(33))}>
+        Increment specific amount
+      </button>
     </div>
   );
 }
-
-export default App;
